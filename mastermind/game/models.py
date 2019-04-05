d326e03ccd3b91ca8e24a377ea7aa6fa02d91c5a
acnouletx <meritxellmm.sd@gmail.com>
2019-04-05 23:55:05 +0200

    changed models

--- mastermind/game/models.py
+++ mastermind/game/models.py
@@ -1,7 +1,28 @@
+import random
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 
-class Peg(models.Model):
+def create_secret_code():
+    """
+    Creates a random secret code of pegs
+    :return: A secret code pegs list
+    """
+    secret_code = []
+
+    for idx in range(Game.NUM_SECRET_PEGS):
+        secret_code.append(random.randint(0, len(Game.COLORS) - 1))
+
+    return secret_code
+
+
+class Game(models.Model):
+    # number of pegs to guess
+    NUM_SECRET_PEGS = 4
+
+    # number of guesses allowed
+    MAX_GUESSES = 11
+
     # define colors
     WHITE = 0
     YELLOW = 1
@@ -24,17 +45,20 @@ class Peg(models.Model):
         (BLACK, 'BLACK'),
     )
 
-    position = models.IntegerField()
-    color = models.IntegerField(choices=COLORS)
+    secret_code = ArrayField(models.IntegerField(choices=COLORS),
+                             size=NUM_SECRET_PEGS,
+                             default=create_secret_code,
+                             editable=False)
 
 
 class Guess(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
     create_date = models.DateTimeField(auto_now_add=True)
-    code_guess = models.ManyToManyField(Peg)
+    code_guess = ArrayField(ArrayField(models.IntegerField(choices=Game.COLORS),
+                                       size=Game.NUM_SECRET_PEGS,
+                                       editable=False))
     correct_positions = models.IntegerField(default=0)
     correct_colors = models.IntegerField(default=0)
 
-
-class Game(models.Model):
-    secret_code = models.ManyToManyField(Peg)
-    guesses = models.ManyToManyField(Guess)
+    class Meta:
+        ordering = ['game', 'create_date']
