b0e11b8c10a27c7459482f3d33d62a7fbfbc7e84
acnouletx <meritxellmm.sd@gmail.com>
2019-04-06 21:19:26 +0200

    added GuessSerializer

--- mastermind/game/serializers.py
+++ mastermind/game/serializers.py
@@ -1,8 +1,24 @@
 from rest_framework import serializers
-from .models import Game
+from .models import Game, Guess
 
 
 class GameSerializer(serializers.ModelSerializer):
     class Meta:
         model = Game
         fields = ["pk"]
+
+
+class GuessSerializer(serializers.ModelSerializer):
+    ERR_MSG_INVALID_CODE = "Guess code is invalid. It must be an string color list of " \
+                           + str(Game.NUM_SECRET_PEGS) + " length."
+
+    code_guess = serializers.ListField(child=serializers.CharField(), required=True)
+
+    def validate_code_guess(self, value):
+        if len(value) != Game.NUM_SECRET_PEGS or not all(n in [color[0] for color in Game.COLORS] for n in value):
+            raise serializers.ValidationError(self.ERR_MSG_INVALID_CODE)
+        return value
+
+    class Meta:
+        model = Guess
+        fields = "__all__"
