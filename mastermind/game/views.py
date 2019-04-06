12929b36fdffe38f5bbe80855158e49e6f5682c0
acnouletx <meritxellmm.sd@gmail.com>
2019-04-06 23:54:04 +0200

    added GuessCode POST request logic

--- mastermind/game/views.py
+++ mastermind/game/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
-from .models import Game
-from .serializers import GameSerializer
+from .models import Game, Guess
+from .serializers import GameSerializer, GuessSerializer
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -10,9 +9,74 @@ class NewGame(APIView):
     def post(self, request):
         """
         Creates new game with secret code
-        :return: game id
+        :return: dict game pk
         """
         game = Game.objects.create()
         serializer = GameSerializer(game)
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class GuessCode(APIView):
+    def post(self, request):
+        """
+        Given a code guess, calculates how many pegs are correct in both color and position,
+        represented by black pegs, and how many pegs have correct color (but wrong position),
+        represented by white pegs.
+        :return: string representing the number of white and black pegs. Ex:'1 black, 2 whites'
+                or string "GAME OVER" if MAX_GUESSES is exceeded
+                or string "YOU WON!!" in case the code was broken
+        """
+        game_id = request.POST.get('game_id', None)
+        code_guess = request.POST.getlist('code_guess', [])
+
+        serializer = GuessSerializer(data={'game': game_id, 'code_guess': code_guess})
+        if serializer.is_valid(raise_exception=True):
+            if Guess.objects.filter(game=game_id).count() > Game.MAX_GUESSES:
+                return Response(Game.GAME_OVER_MSG, status=status.HTTP_200_OK)
+            else:
+                guess = serializer.save()
+                black_pegs, white_pegs = self.get_correct_pegs(guess)
+                if black_pegs == Game.NUM_SECRET_PEGS:
+                    return Response(Game.WINNIG_MSG, status=status.HTTP_200_OK)
+                return Response(self.get_guess_response(black_pegs, white_pegs), status=status.HTTP_200_OK)
+
+    @staticmethod
+    def get_correct_pegs(guess):
+        """
+        Get number black and white pegs
+        :param guess: Guess instance of current guess
+        :return: int black_pegs, int white_pegs
+        """
+        secret_code = guess.game.secret_code
+        code_guess = guess.code_guess
+        black_pegs = 0
+        white_pegs = 0
+        for idx, color in enumerate(code_guess):
+            if secret_code[idx] == color:
+                black_pegs += 1
+            elif secret_code[idx] in code_guess:
+                white_pegs += 1
+        guess.black_pegs = black_pegs
+        guess.white_pegs = white_pegs
+        guess.save()
+        return black_pegs, white_pegs
+
+    @staticmethod
+    def plural_pegs(small_pegs):
+        """
+        Get plural char 's' if needed
+        :param small_pegs: number of pegs
+        :return: string 's' or empty string
+        """
+        return 's' if small_pegs > 1 else ''
+
+    def get_guess_response(self, black_pegs, white_pegs):
+        """
+        Get string response given a guess code
+        :param black_pegs: number of black pegs
+        :param white_pegs: number of white pegs
+        :return: string
+        """
+        return "{0} black{1}, {2} white{3}".format(black_pegs, self.plural_pegs(black_pegs),
+                                                   white_pegs, self.plural_pegs(white_pegs))
